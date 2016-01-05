4fd1f699117bf30f755c3266005aeb1f9ccd2be0
Greg Simon <gregsimon@gmail.com>
2016-01-05 13:30:49 -0800

    add gtk-3-dev to the linux reqs

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -112,13 +112,13 @@ dev_list="$dev_list bison cdbs curl dpkg-dev elfutils devscripts fakeroot
           libasound2-dev libbrlapi-dev libav-tools
           libbz2-dev libcairo2-dev libcap-dev libcups2-dev libcurl4-gnutls-dev
           libdrm-dev libelf-dev libexif-dev libgconf2-dev libglib2.0-dev
-          libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev libkrb5-dev
-          libnspr4-dev libnss3-dev libpam0g-dev libpci-dev libpulse-dev
-          libsctp-dev libspeechd-dev libsqlite3-dev libssl-dev libudev-dev
-          libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev openbox
-          patch perl php5-cgi pkg-config python python-cherrypy3 python-crypto
-          python-dev python-numpy python-opencv python-openssl python-psutil
-          python-yaml rpm ruby subversion ttf-dejavu-core
+          libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev libgtk-3-dev
+          libkrb5-dev libnspr4-dev libnss3-dev libpam0g-dev libpci-dev
+          libpulse-dev libsctp-dev libspeechd-dev libsqlite3-dev libssl-dev
+          libudev-dev libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev
+          openbox patch perl php5-cgi pkg-config python python-cherrypy3
+          python-crypto python-dev python-numpy python-opencv python-openssl
+          python-psutil python-yaml rpm ruby subversion ttf-dejavu-core
           ttf-kochi-gothic ttf-kochi-mincho wdiff zip
           $chromeos_dev_list"
 
